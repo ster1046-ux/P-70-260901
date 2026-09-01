245db91a153216ea0bb818473e749e48500701af
ster1046-ux <ster1046@gmail.com>
2026-09-01 09:50:19 +0900

    feat: Member 엔티티 생성# with '#' will be ignored, and an empty message aborts the commit.

---
+++ back/src/main/java/com/back/p67260811/domain/member/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.back.p67260811.domain.member.repository;
+
+
+import com.back.p67260811.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
--- back/src/main/java/com/back/p67260811/global/initData/BaseInitData.java
+++ back/src/main/java/com/back/p67260811/global/initData/BaseInitData.java
@@ -1,5 +1,6 @@
 package com.back.p67260811.global.initData;
 
+import com.back.p67260811.domain.member.service.MemberService;
 import com.back.p67260811.domain.post.post.entity.Post;
 import com.back.p67260811.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
@@ -18,15 +19,18 @@ public class BaseInitData {
     @Lazy
     private BaseInitData self;
     private final PostService postService;
+    private final MemberService memberService;
+
     @Bean
     ApplicationRunner initDataRunner() {
         return args -> {
             self.work1();
+            self.work2();
         };
     }
 
     @Transactional
-    public void work1() {
+    public void work2() {
         if (postService.count() > 0) {
             return;
         }
@@ -42,4 +46,17 @@ public class BaseInitData {
         post2.addComment("댓글 2-2");
     }
 
+    @Transactional
+    public void work1() {
+        if (memberService.count() > 0) {
+            return;
+        }
+
+        memberService.join("system", "system", "시스템");
+        memberService.join("admin", "admin", "운영자");
+        memberService.join("user1", "1234", "유저1");
+        memberService.join("user2", "1234", "유저2");
+        memberService.join("user3", "1234", "유저3");
+
+    }
 }
